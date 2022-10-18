89b133c25df510a48016fd3f74bfa6d51faf63b7
Komepraise <komepraise@gmail.com>
2022-10-18 23:14:50 +0000

    c2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,21 @@
-#include <main.h>
-int main()
+#include "main.h"
+/**
+ * This is a program to 
+ * print _putchar
+ * and return 0
+ */
+
+int main(void)
 {
-printf("_putchar");
-return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
