643a8f2ef92f26e6d340d1b0875b890b5688e7c0
Komepraise <komepraise@gmail.com>
2022-10-25 03:13:27 +0000

    c5

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,11 @@
  * @s: this is the input string
  * Return: Lenngth of the string
  */
- int _strlen(char *s)
+int _strlen(char *s)
 {
 int index;
-for (index = 0; s[index] != '\0' ; index++);
+for
+(index = 0; s[index] != '\0' ; index++);
 return (index);
 }
 
