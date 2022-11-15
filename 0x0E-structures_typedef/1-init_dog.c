06827fcc152b4285e5a2a360a2dddf23ee66d1a6
Komepraise <komepraise@gmail.com>
2022-11-15 01:50:57 +0000

    fixed

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+*
+*dog - The structure name
+*@name: the name of the dog
+*@age: the age of the dog
+*@owner: the owner of the dog
+*/
+
+void init_dog()
+{
+char *name;
+float age;
+char *ownwer;
+};
+
