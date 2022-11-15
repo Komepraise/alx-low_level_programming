37ce4cbbd38b1268df24510a6edffd9f874e236d
Komepraise <komepraise@gmail.com>
2022-11-15 02:43:30 +0000

    test

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
 #include "dog.h"
 
 /**
+*main - entry point
+*init_dog: the prototype
+*d: another paramerter
 *dog - The structure name
 *@name: the name of the dog
 *@age: the age of the dog
@@ -12,5 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 struct dog dog;
 
+}
+void main(void)
+{
 }
 
