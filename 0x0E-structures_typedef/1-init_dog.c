dadd42831b0df91e5ad2ef0bd6a143610f9ca659
Komepraise <komepraise@gmail.com>
2022-11-15 02:15:47 +0000

    var

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,17 +2,15 @@
 #include "dog.h"
 
 /**
-*
 *dog - The structure name
 *@name: the name of the dog
 *@age: the age of the dog
 *@owner: the owner of the dog
 */
 
-void init_dog()
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-char *name;
-float age;
-char *ownwer;
-};
+struct dog dog;
+
+}
 
