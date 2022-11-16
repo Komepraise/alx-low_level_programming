1dcacc8774750959fd9560fa77fe97691b060d8e
Komepraise <komepraise@gmail.com>
2022-11-16 05:50:18 +0100

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- * struct dog - A structure name
- * @name: The dog's name
- * @age: The dog's age
- * @owner: THe dog's owner
- */
+
 struct dog/
 {
 	char *name;
