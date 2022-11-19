81ae3102f25227aec6c4843a11568f851e74e3a1
Komepraise <komepraise@gmail.com>
2022-11-19 17:50:41 +0000

    c4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - a funtion to print number
+ * @i: int the number
+ * Returns: 0
+ */
+
+void print_numbers(void)
+int i;
+{
+	if
+	(i = 0; i < 10;i++)
+	_putchar(i);
+	else
+	return 0;
+}
+
