21fbd8d7b2f3f022266ee7619843658715e0b717
Komepraise <komepraise@gmail.com>
2022-11-20 13:06:37 +0000

    c4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,21 @@
 
 /**
  * print_numbers - a funtion to print number
- * @i: int the number
  * Returns: 0
  */
 
 void print_numbers(void)
 {
 	int i;
+
 	for
-	(i = 0;i < 10;i++)
+
+	(i = 0;
+	 i < 10;
+	 i++)
+
 	_putchar(i);
+
 	_putchar('\n');
 }
 
