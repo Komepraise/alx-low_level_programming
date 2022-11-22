dffd20439a8c8664ae7ded2a0de7ca3dd96d51dc
Komepraise <komepraise@gmail.com>
2022-11-22 03:32:00 +0000

    c5

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_diagonal - THe function name
+ * @n: Parameter
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+	int i;
+
+	if
+
+		(n <= 0)
+
+			_putchar('\n');
+	else
+	{
+		for
+			(x = 0;
+			 x < n;
+			 x++)
+			{
+				for
+					(i = 0;
+					 i <= x;
+					 i++);
+				_putchar(92);
+				_putchar('\n');
+			}
+		_putchar('\n');
+	}
+}
