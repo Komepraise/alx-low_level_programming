3a4c89c447a55e8955cff98785d474acc646848a
Komepraise <komepraise@gmail.com>
2022-11-26 08:38:16 +0000

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,12 +5,11 @@
  */
 void more_numbers(void)
 {
-	int x = 0;
-	int y = 0;
+	int y, x = 0;
 
-	for (; x < 10; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (; y < 15; y++)
+		for (y = 0; y < 15; y++)
 		{
 			if (y >= 10)
 			_putchar((y / 10) + '0');
