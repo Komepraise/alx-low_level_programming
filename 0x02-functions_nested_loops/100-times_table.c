6f7456f46d5a45ffdca799848a9e0485c51851bd
Komepraise <komepraise@gmail.com>
2023-03-09 18:06:00 +0000

    catch up

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,10 +6,44 @@
  */
 void print_times_table(int n)
 {
-if
-(n < 15 || n > 0);
-_putchar(n);
-else
-_putchar('O');
-return (0);
+	int x, y, z;
+
+	if
+
+		(n < 15 || n > 0);
+	{
+		int x, y, z;
+
+		for (x = 0;
+			 x <= 9;
+			 x++)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			for (y = 1;
+				 y <= 9;
+				 y++)
+			{
+				z = (x * y);
+				if
+					((z / 10) > 0)
+				{
+					_putchar((z / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar((z % 10) + '0');
+				if
+					(y < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
