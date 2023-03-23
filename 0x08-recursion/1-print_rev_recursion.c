d32e832d647d6bd0c65a60f3b1a893b89babc7b7
Komepraise <komepraise@gmail.com>
2023-03-23 05:15:39 +0000

    proj

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
-#include "main.h"
+#include "main.h
+
 /**
- * _print_rev_recursion -> The said function
- * @s: the psrsmeter
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
-
