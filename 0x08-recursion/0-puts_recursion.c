87444c1ae3ee91df1de62da6bc6a0412e38e6ef1
Komepraise <komepraise@gmail.com>
2023-06-07 09:00:58 +0100

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
+//Done by Oghenekome Praise
+
+/*
+ *
  */
-void _puts_recursion(char *s)
-{
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
 
-	else
-		_putchar('\n');
+void _puts_recursion(char *s)
+	for (s=1; s < '\n'; s=s++)
+{
+	putchar(s);
 }
+
+
+
