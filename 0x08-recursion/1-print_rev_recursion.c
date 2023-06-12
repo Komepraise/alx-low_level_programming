4ba40646d277271f11cd51adadd61df24ee53143
Komepraise <komepraise@gmail.com>
2023-06-12 14:04:47 +0000

    getting fustrated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,13 +6,15 @@
  */
 void _print_rev_recursion(char *s)
 {
+	{
+		while (*s != '\0')
+	++s;
+	}
 	if (*s)
 	{
-		_print_rev_recursion(s + 1);
-	}
-	if ('\n')
-	{
-		_print_rev_recursion(s - 1);
 		_putchar(*s);
+		_print_rev_recursion(s - 1);
 	}
+	else
+		_putchar('\n');
 }
