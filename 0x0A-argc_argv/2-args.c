6a69f9e00119035a5f035f7b6667e6036dfe2e7a
Komepraise <komepraise@gmail.com>
2023-06-23 04:30:51 +0000

    argc and v

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints recieved arguments
+ * @argc - the number of arguments
+ * @argv: the array of arguments
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int var;
+	for (var = 0; var < argc; var++)
+	{
+		printf("%s\n", argv[var]);
+	}
+	return (0);
+}
+
