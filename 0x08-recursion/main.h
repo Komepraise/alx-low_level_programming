0fa9975059239e0e5ab16dae33da5de8fb2d39da
Komepraise <komepraise@gmail.com>
2023-07-03 06:28:55 +0000

    squrt

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 void _puts_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
+
 
 #endif
