b9728d10594bcd4be5842eb6d632a82f454c71f3
komepraise <komepraise@gmail.com>
2024-04-14 07:05:10 +0000

    done

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,9 @@
  * mul - multiplies two integers.
  * @a: The first integer to be multiplied.
  * @b: The second integer to be multiplied.
- * @c: The recult of the multiplication.
+ *
  */
 int mul(int a, int b)
 {
-	int c;
-	c = a * b;
-	return (c);
+	return (a * b);
 }
